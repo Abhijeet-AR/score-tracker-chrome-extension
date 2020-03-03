410b8463b13f72001b1368c6bdca6863219395dd
Abhijeet Reddy <abhijeet_abhi@live.co.uk>
2020-03-03 19:58:17 +0530

    Score display changed

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+console.log('Opened!');
 let user_info = {
     'codechef': 'abhijeet_ar',
     'codeforces': 'abhijeet_ar',
@@ -16,26 +17,54 @@ async function fill_scores(){
     const codeforces_url = get_url('codeforces');
     fetch(codeforces_url)
     .then(response => response.json())
-    .then(data => document.getElementById('codeforces').textContent = data.rating)
+    .then(data =>{
+        if(data.status == 'Success')
+            document.getElementById('codeforces').textContent = data.rating;
+        
+        else
+        document.getElementById('codeforces').textContent = 'error';
+    })
     .catch(() => console.log('Codeforces Error!'));
     
     const codechef_url = get_url('codechef');
     fetch(codechef_url)
     .then(response => response.json())
-    .then(data => document.getElementById('codechef').textContent = data.rank)
+    .then(data => {
+        if(data.status == 'Success')
+            document.getElementById('codechef').textContent = data.rank;
+
+        else 
+        document.getElementById('codechef').textContent = 'error';
+    })
     .catch(() => console.log('Codechef Error!'));
 
     const spoj_url = get_url('spoj');
     fetch(spoj_url)
     .then(response => response.json())
-    .then(data => document.getElementById('spoj').textContent = data.points)
+    .then(data => {
+        if(data.points)
+            document.getElementById('spoj').textContent = data.points;
+
+        else
+        document.getElementById('spoj').textContent = 'error';
+    })
     .catch(() => console.log('SPOJ Error!'));
 
     const interviewbit_url = get_url('interviewbit');
     fetch(interviewbit_url)
     .then(response => response.json())
-    .then(data => document.getElementById('interviewbit').textContent = data.score)
-    .catch(() => console.log('Interviewbit Error!'));
+    .then(data => {
+        if(data.status == 'Success')
+            document.getElementById('interviewbit').textContent = data.score;
+        
+        else
+            document.getElementById('interviewbit').textContent = 'error';
+    })
+    .catch(() => 
+    {
+        console.log('Interviewbit Error');
+        document.getElementById('interviewbit').textContent = 'error';
+    })
 }
 
 fill_scores()
