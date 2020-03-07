4044eb3997cba999f6e7c0bc56b7d7efe181011d
Abhijeet Reddy <abhijeet_abhi@live.co.uk>
2020-03-07 23:58:51 +0530

    Added storage of scores in extension

--- popup.js
+++ popup.js
@@ -18,23 +18,44 @@ async function fill_scores(){
     fetch(codeforces_url)
     .then(response => response.json())
     .then(data =>{
-        if(data.status == 'Success')
-            document.getElementById('codeforces').textContent = data.rating;
-        
+        if(data.status == 'Success'){
+            if(data.status == 'Success'){
+                chrome.storage.sync.get('codeforces', function(stored_data){
+                    try{
+                        document.getElementById('codeforces').textContent = data.rating - stored_data.codechef.rating;
+                    }
+                    catch(err){
+                        chrome.storage.sync.set({'codeforces': data}, function(){
+                            document.getElementById('codeforces').textContent = 0;
+                        });
+                    }
+                });
+            }
+        }
         else
-        document.getElementById('codeforces').textContent = 'error';
+            document.getElementById('codeforces').textContent = 'error';
     })
     .catch(() => console.log('Codeforces Error!'));
     
     const codechef_url = get_url('codechef');
     fetch(codechef_url)
     .then(response => response.json())
-    .then(data => {
-        if(data.status == 'Success')
-            document.getElementById('codechef').textContent = data.rank;
-
-        else 
-        document.getElementById('codechef').textContent = 'error';
+    .then(data => {     
+        if(data.status == 'Success'){
+            chrome.storage.sync.get('codechef', function(stored_data){
+                console.log(data);
+                try{
+                    document.getElementById('codechef').textContent = data.rank - stored_data.codechef.rank;
+                }
+                catch(err){
+                    chrome.storage.sync.set({'codechef': data}, function(){
+                        document.getElementById('codechef').textContent = 0;
+                    });
+                }
+            });
+        }
+        else
+            document.getElementById('codechef').textContent = 'error';
     })
     .catch(() => console.log('Codechef Error!'));
 
@@ -42,11 +63,21 @@ async function fill_scores(){
     fetch(spoj_url)
     .then(response => response.json())
     .then(data => {
-        if(data.points)
-            document.getElementById('spoj').textContent = data.points;
-
+        if(data.status == 'Success'){
+            chrome.storage.sync.get('spoj', function(stored_data){
+                console.log(data);
+                try{
+                    document.getElementById('spoj').textContent = data.points - stored_data.spoj.points;
+                }
+                catch(err){
+                    chrome.storage.sync.set({'spoj': data}, function(){
+                        document.getElementById('spoj').textContent = 0;
+                    });
+                }
+            });
+        }
         else
-        document.getElementById('spoj').textContent = 'error';
+            document.getElementById('spoj').textContent = 'error';
     })
     .catch(() => console.log('SPOJ Error!'));
 
@@ -54,11 +85,19 @@ async function fill_scores(){
     fetch(interviewbit_url)
     .then(response => response.json())
     .then(data => {
-        if(data.status == 'Success')
-            document.getElementById('interviewbit').textContent = data.score;
-        
-        else
-            document.getElementById('interviewbit').textContent = 'error';
+        if(data.status == 'Success'){
+            chrome.storage.sync.get('interviewbit', function(stored_data){
+                console.log(data);
+                try{
+                    document.getElementById('interviewbit').textContent = data.score - stored_data.interviewbit.score;
+                }
+                catch(err){
+                    chrome.storage.sync.set({'interviewbit': data}, function(){
+                        document.getElementById('interviewbit').textContent = 0;
+                    });
+                }
+            });
+        }
     })
     .catch(() => 
     {
