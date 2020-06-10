c261767965fe0167e1b6dd060a85ecc3e87e01ff
Abhijeet Reddy <abhijeet_abhi@live.co.uk>
2020-06-10 13:28:57 +0530

    changed logo and minor updates

--- background.js
+++ background.js
@@ -1,240 +1,240 @@
-console.log('background Running');
-
-let reset_time = new Date().setHours(0, 5);
+console.log("background Running");
 
 let user_info = {
-    'codechef': 'abhijeet_ar',
-    'codeforces': 'abhijeet_ar',
-    'spoj': 'abhijeet_ar',
-    'interviewbit': 'abhijeet_ar'
-}
+  codechef: "abhijeet_ar",
+  codeforces: "abhijeet_ar",
+  spoj: "abhijeet_ar",
+  interviewbit: "abhijeet_ar"
+};
 
-function get_url(platform){
-    return 'http://competitive-coding-api.herokuapp.com/api/'+platform+'/'+user_info[platform];
+let reset_time_info = {
+  hours: 1,
+  minutes: 14
+};
+
+let reset_time = new Date().setHours(
+  reset_time_info.hours,
+  reset_time_info.minutes
+);
+
+function get_url(platform) {
+  return (
+    "http://competitive-coding-api.herokuapp.com/api/" +
+    platform +
+    "/" +
+    user_info[platform]
+  );
 }
 
 function reset_data() {
-    const codeforces_url = get_url('codeforces');
+  const codeforces_url = get_url("codeforces");
 
-    fetch(codeforces_url)
+  fetch(codeforces_url)
     .then(response => response.json())
     .then(data => {
-        if (data.status == 'Success'){
-            chrome.storage.sync.get(['date', 'count', 'codeforces'], (stored_data) => {
-                try{
-                    let d = new Date();
-                    let date_today = {
-                        'date': d.getDate(),
-                        'month': d.getMonth(),
-                        'year': d.getYear()
+      if (data.status == "Success") {
+        chrome.storage.sync.get(
+          ["date", "count", "codeforces"],
+          stored_data => {
+            try {
+              let d = new Date();
+              let date_today = {
+                date: d.getDate(),
+                month: d.getMonth(),
+                year: d.getYear()
+              };
+
+              // console.log('Stored Data', stored_data, date_today);
+              if (
+                stored_data.codeforces == undefined ||
+                JSON.stringify(stored_data.date) != JSON.stringify(date_today)
+              ) {
+                chrome.storage.sync.set({ codeforces: data }, function() {
+                  try {
+                    if (++count == 4) {
+                      chrome.storage.sync.set({ date: date_today }, () => {
+                        console.log("Date updated");
+                      });
                     }
+                  } catch (err) {
+                    count = 0;
+                  }
 
-                    // console.log('Stored Data', stored_data, date_today);
-                    if((stored_data.codeforces == undefined) || JSON.stringify(stored_data.date) != JSON.stringify(date_today)){
-                        chrome.storage.sync.set({'codeforces': data}, function(){
-                            try{
-                                if(++count == 4){
-                                    chrome.storage.sync.set({'date': date_today, }, () => {
-                                        console.log('Date updated');
-                                    })
-                                }
-                            }   
-                            catch(err){
-                                count = 0;
-                            }
+                  console.log("Codeforces Data Set");
+                });
+              } else console.log("Codeforces Data Already set");
+            } catch (err) {
+              console.log("Codeforces Error", err);
+            }
+          }
+        );
+      } else console.log("Codeforces Error");
+    });
 
-                            console.log('Codeforces Data Set');
-                        });
-                    }
+  const codechef_url = get_url("codechef");
 
-                    else
-                        console.log('Codeforces Data Already set');
-                }
-
-                catch(err){
-                    console.log('Codeforces Error', err);
-                }
-            });
-        }
-
-        else
-            console.log('Codeforces Error');
-    })
-
-    const codechef_url = get_url('codechef');
-
-    fetch(codechef_url)
+  fetch(codechef_url)
     .then(response => response.json())
     .then(data => {
-        if (data.status == 'Success'){
-            chrome.storage.sync.get(['date', 'codechef'], (stored_data) => {
-                try{
-                    let d = new Date();
-                    let date_today = {
-                        'date': d.getDate(),
-                        'month': d.getMonth(),
-                        'year': d.getYear()
+      if (data.status == "Success") {
+        chrome.storage.sync.get(["date", "codechef"], stored_data => {
+          try {
+            let d = new Date();
+            let date_today = {
+              date: d.getDate(),
+              month: d.getMonth(),
+              year: d.getYear()
+            };
+
+            // console.log('Stored Data', stored_data.codechef);
+            if (
+              stored_data.codechef == undefined ||
+              JSON.stringify(stored_data.date) != JSON.stringify(date_today)
+            ) {
+              chrome.storage.sync.set(
+                {
+                  codechef: {
+                    status: data.status,
+                    rank: data.rank,
+                    rating: data.rating,
+                    global_rank: data.global_rank,
+                    contests: data.contests
+                  }
+                },
+                function() {
+                  try {
+                    if (++count == 4) {
+                      chrome.storage.sync.set({ date: date_today }, () => {
+                        console.log("Date updated");
+                      });
                     }
+                  } catch (err) {
+                    count = 0;
+                  }
 
-                    // console.log('Stored Data', stored_data.codechef);
-                    if((stored_data.codechef == undefined) || (JSON.stringify(stored_data.date) != JSON.stringify(date_today))){
-                        chrome.storage.sync.set({'codechef': {
-                            'status': data.status, 
-                            'rank': data.rank,
-                            'rating': data.rating,
-                            'global_rank': data.global_rank,
-                            'contests': data.contests
-                        }
-                        }, function(){
-                            try{
-                                if(++count == 4){
-                                    chrome.storage.sync.set({'date': date_today, }, () => {
-                                        console.log('Date updated');
-                                    })
-                                }
-                            }
-                            catch(err){
-                                count = 0;
-                            }
-
-                            console.log('Codechef Data Set');
-                        });
-                    }
-
-                    else
-                        console.log('Codechef Data Already set');
+                  console.log("Codechef Data Set");
                 }
+              );
+            } else console.log("Codechef Data Already set");
+          } catch (err) {
+            console.log("Codechef Error", err);
+          }
+        });
+      } else console.log("Codechef Error");
+    });
 
-                catch(err){
-                    console.log('Codechef Error', err);
-                }
-            });
-        }
+  const spoj_url = get_url("spoj");
 
-        else
-            console.log('Codechef Error');
-    })
-
-    const spoj_url = get_url('spoj');
-
-    fetch(spoj_url)
+  fetch(spoj_url)
     .then(response => response.json())
     .then(data => {
-        if (data.status == 'Success'){
-            chrome.storage.sync.get(['date', 'spoj'], (stored_data) => {
-                try{
-                    let d = new Date();
-                    let date_today = {
-                        'date': d.getDate(),
-                        'month': d.getMonth(),
-                        'year': d.getYear()
-                    }
+      if (data.status == "Success") {
+        chrome.storage.sync.get(["date", "spoj"], stored_data => {
+          try {
+            let d = new Date();
+            let date_today = {
+              date: d.getDate(),
+              month: d.getMonth(),
+              year: d.getYear()
+            };
 
-                    // console.log('Stored Data', stored_data);
-                    if((stored_data.spoj == undefined) || (JSON.stringify(stored_data.date) != JSON.stringify(date_today))){
-                        chrome.storage.sync.set({'spoj': data}, () => {
-                            try{
-                                if(++count == 4){
-                                    chrome.storage.sync.set({'date': date_today, }, () => {
-                                        console.log('Date updated');
-                                    })
-                                }
-                            }
-                            catch(err){
-                                count = 0;
-                            }
-
-                            console.log('SPOJ Data Set');
-                        });
-                    }
-
-                    else
-                        console.log('SPOJ Data Already set');
+            // console.log('Stored Data', stored_data);
+            if (
+              stored_data.spoj == undefined ||
+              JSON.stringify(stored_data.date) != JSON.stringify(date_today)
+            ) {
+              chrome.storage.sync.set({ spoj: data }, () => {
+                try {
+                  if (++count == 4) {
+                    chrome.storage.sync.set({ date: date_today }, () => {
+                      console.log("Date updated");
+                    });
+                  }
+                } catch (err) {
+                  count = 0;
                 }
 
-                catch(err){
-                    console.log('SPOJ Error', err);
-                }
-            });
-        }
+                console.log("SPOJ Data Set");
+              });
+            } else console.log("SPOJ Data Already set");
+          } catch (err) {
+            console.log("SPOJ Error", err);
+          }
+        });
+      } else console.log("SPOJ Error");
+    });
 
-        else
-            console.log('SPOJ Error');
-    })
+  const interviewbit_url = get_url("interviewbit");
 
-    const interviewbit_url = get_url('interviewbit');
-
-    fetch(interviewbit_url)
+  fetch(interviewbit_url)
     .then(response => response.json())
     .then(data => {
-        if (data.status == 'Success'){
-            chrome.storage.sync.get(['date', 'interviewbit'], (stored_data) => {
-                try{
-                    let d = new Date();
-                    let date_today = {
-                        'date': d.getDate(),
-                        'month': d.getMonth(),
-                        'year': d.getYear()
-                    }
+      if (data.status == "Success") {
+        chrome.storage.sync.get(["date", "interviewbit"], stored_data => {
+          try {
+            let d = new Date();
+            let date_today = {
+              date: d.getDate(),
+              month: d.getMonth(),
+              year: d.getYear()
+            };
 
-                    // console.log('Stored Data', stored_data);
-                    if((stored_data.interviewbit == undefined) || (JSON.stringify(stored_data.date) != JSON.stringify(date_today))){
-                        chrome.storage.sync.set({'interviewbit': data}, () => {
-                            try{
-                                if(++count == 4){
-                                    chrome.storage.sync.set({'date': date_today, }, () => {
-                                        console.log('Date updated');
-                                    })
-                                }
-                            }
-                            catch(err){
-                                count = 0;
-                            }
-
-                            console.log('Interviewbit Data Set');
-                        });
-                    }
-
-                    else
-                        console.log('Interviewbit Data Already set');
+            // console.log('Stored Data', stored_data);
+            if (
+              stored_data.interviewbit == undefined ||
+              JSON.stringify(stored_data.date) != JSON.stringify(date_today)
+            ) {
+              chrome.storage.sync.set({ interviewbit: data }, () => {
+                try {
+                  if (++count == 4) {
+                    chrome.storage.sync.set({ date: date_today }, () => {
+                      console.log("Date updated");
+                    });
+                  }
+                } catch (err) {
+                  count = 0;
                 }
 
-                catch(err){
-                    console.log('Interviewbit Error', err);
-                }
-            });
-        }
-
-        else
-            console.log('Interviewbit Error');
-    })
-}
-
-function create_alarm(){
-    chrome.alarms.get('Reset', (alarm) => {
-        if(alarm)
-            console.log('Alarm already exists', alarm);
-
-        else{
-            chrome.alarms.create('Reset', {when: reset_time});
-            console.log('Alarm created at ', new Date().getHours(), ':', new Date().getMinutes());
-            // let today_time = ;
-            reset_time = new Date().setHours(0, 5)+86400000;
-        }
+                console.log("Interviewbit Data Set");
+              });
+            } else console.log("Interviewbit Data Already set");
+          } catch (err) {
+            console.log("Interviewbit Error", err);
+          }
+        });
+      } else console.log("Interviewbit Error");
     });
 }
 
-chrome.runtime.onInstalled.addListener((details) => {
-    console.log(details.reason, 'successful');  
-    create_alarm();
-})
+function create_alarm() {
+  chrome.alarms.get("Reset", alarm => {
+    if (alarm) console.log("Alarm already exists", alarm);
+    else {
+      chrome.alarms.create("Reset", { when: reset_time });
+      console.log(
+        "Alarm created at ",
+        new Date().getHours(),
+        ":",
+        new Date().getMinutes()
+      );
+      reset_time =
+        new Date().setHours(reset_time_info.hours, reset_time_info.minutes) +
+        86400000;
+    }
+  });
+}
 
-chrome.runtime.onStartup.addListener(() => {
-    create_alarm();
-});
+chrome.runtime.onInstalled.addListener(create_alarm);
+chrome.runtime.onStartup.addListener(create_alarm);
 
 chrome.alarms.onAlarm.addListener(() => {
-    console.log("Resetting Data at ", new Date().getHours(), ':', new Date().getMinutes());
-    reset_data();
-    create_alarm();
+  console.log(
+    "Resetting Data at ",
+    new Date().getHours(),
+    ":",
+    new Date().getMinutes()
+  );
+  reset_data();
+
+  setTimeout(create_alarm, 5000);
 });
--- popup.js
+++ popup.js
@@ -1,122 +1,139 @@
-console.log('Opened!');
+console.log("Opened!");
 let user_info = {
-    'codechef': 'abhijeet_ar',
-    'codeforces': 'abhijeet_ar',
-    'spoj': 'abhijeet_ar',
-    'interviewbit': 'abhijeet_ar'
-}
+  codechef: "abhijeet_ar",
+  codeforces: "abhijeet_ar",
+  spoj: "abhijeet_ar",
+  interviewbit: "abhijeet_ar"
+};
 
 // const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
-function get_url(platform){
-    return 'http://competitive-coding-api.herokuapp.com/api/'+platform+'/'+user_info[platform];
+function get_url(platform) {
+  return (
+    "http://competitive-coding-api.herokuapp.com/api/" +
+    platform +
+    "/" +
+    user_info[platform]
+  );
 }
 
-async function fill_scores(){
- 
-    const codeforces_url = get_url('codeforces');
-    fetch(codeforces_url)
-    .then(response => response.json())
-    .then(data =>{
-        if(data.status == 'Success'){
-            chrome.storage.sync.get('codeforces', function(stored_data){
-                try{
-                    document.getElementById('codeforces').textContent = data.rating - stored_data.codechef.rating;
-                }
-                catch(err){
-                    chrome.storage.sync.set({'codeforces': data}, function(){
-                        document.getElementById('codeforces').textContent = 0;
-                    });
-                }
-            });
-        }
-
-        else
-            document.getElementById('codeforces').textContent = 'error';
-    })
-    .catch(() => console.log('Codeforces Error!'));
-    
-    const codechef_url = get_url('codechef');
-    fetch(codechef_url)
-    .then(response => response.json())
-    .then(data => {     
-        if(data.status == 'Success'){
-            chrome.storage.sync.get('codechef', function(stored_data){
-                // console.log(data);
-                try{
-                    document.getElementById('codechef').textContent = data.rank - stored_data.codechef.rank;
-                }
-                catch(err){
-                    chrome.storage.sync.set({'codechef': 
-                    {
-                        'status': data.status, 
-                        'rank': data.rank,
-                        'rating': data.rating,
-                        'global_rank': data.global_rank,
-                        'contests': data.contests
-                    }
-                    }, function(){
-                        document.getElementById('codechef').textContent = 0;
-                    });
-                }
-            });
-        }
-        else
-            document.getElementById('codechef').textContent = 'error';
-    })
-    .catch(() => console.log('Codechef Error!'));
-
-    const spoj_url = get_url('spoj');
-    fetch(spoj_url)
+async function fill_scores() {
+  const codeforces_url = get_url("codeforces");
+  fetch(codeforces_url)
     .then(response => response.json())
     .then(data => {
-        if(data.status == 'Success'){
-            chrome.storage.sync.get('spoj', function(stored_data){
-                // console.log(data);
-                try{
-                    document.getElementById('spoj').textContent = data.points - stored_data.spoj.points;
-                }
-                catch(err){
-                    chrome.storage.sync.set({'spoj': data}, function(){
-                        document.getElementById('spoj').textContent = 0;
-                    });
-                }
+      if (data.status == "Success") {
+        chrome.storage.sync.get("codeforces", function(stored_data) {
+          try {
+            document.getElementById("codeforces").textContent =
+              data.rating - stored_data.codechef.rating;
+          } catch (err) {
+            chrome.storage.sync.set({ codeforces: data }, function() {
+              document.getElementById("codeforces").textContent = 0;
             });
-        }
-        else
-            document.getElementById('spoj').textContent = 'error';
+          }
+        });
+      } else document.getElementById("codeforces").textContent = "error";
     })
-    .catch(() => console.log('SPOJ Error!'));
+    .catch(() => console.log("Codeforces Error!"));
 
-    const interviewbit_url = get_url('interviewbit');
-    fetch(interviewbit_url)
+  const codechef_url = get_url("codechef");
+  fetch(codechef_url)
     .then(response => response.json())
     .then(data => {
-        if(data.status == 'Success'){
-            chrome.storage.sync.get('interviewbit', function(stored_data){
-                // console.log(data);
-                try{
-                    document.getElementById('interviewbit').textContent = data.score - stored_data.interviewbit.score;
+      if (data.status == "Success") {
+        chrome.storage.sync.get("codechef", function(stored_data) {
+          // console.log(data);
+          try {
+            document.getElementById("codechef").textContent =
+              data.rank - stored_data.codechef.rank;
+          } catch (err) {
+            chrome.storage.sync.set(
+              {
+                codechef: {
+                  status: data.status,
+                  rank: data.rank,
+                  rating: data.rating,
+                  global_rank: data.global_rank,
+                  contests: data.contests
                 }
-                catch(err){
-                    chrome.storage.sync.set({'interviewbit': data}, function(){
-                        document.getElementById('interviewbit').textContent = 0;
-                    });
-                }
+              },
+              function() {
+                document.getElementById("codechef").textContent = 0;
+              }
+            );
+          }
+        });
+      } else document.getElementById("codechef").textContent = "error";
+    })
+    .catch(() => console.log("Codechef Error!"));
+
+  const spoj_url = get_url("spoj");
+  fetch(spoj_url)
+    .then(response => response.json())
+    .then(data => {
+      if (data.status == "Success") {
+        chrome.storage.sync.get("spoj", function(stored_data) {
+          // console.log(data);
+          try {
+            document.getElementById("spoj").textContent =
+              data.points - stored_data.spoj.points;
+          } catch (err) {
+            chrome.storage.sync.set({ spoj: data }, function() {
+              document.getElementById("spoj").textContent = 0;
             });
-        }
+          }
+        });
+      } else document.getElementById("spoj").textContent = "error";
     })
-    .catch(() => 
-    {
-        console.log('Interviewbit Error');
-        document.getElementById('interviewbit').textContent = 'error';
+    .catch(() => console.log("SPOJ Error!"));
+
+  const interviewbit_url = get_url("interviewbit");
+  fetch(interviewbit_url)
+    .then(response => response.json())
+    .then(data => {
+      if (data.status == "Success") {
+        chrome.storage.sync.get("interviewbit", function(stored_data) {
+          // console.log(data);
+          try {
+            document.getElementById("interviewbit").textContent =
+              data.score - stored_data.interviewbit.score;
+          } catch (err) {
+            chrome.storage.sync.set({ interviewbit: data }, function() {
+              document.getElementById("interviewbit").textContent = 0;
+            });
+          }
+        });
+      }
     })
+    .catch(() => {
+      console.log("Interviewbit Error");
+      document.getElementById("interviewbit").textContent = "error";
+    });
 }
 
-fill_scores()
+fill_scores();
 
 // window.onload = function() {
 //     document.querySelector('#settings').addEventListener('click', () => {
 //         chrome.runtime.openOptionsPage();
 //     });
 // }
+
+window.onload = function() {
+  document.getElementById("settings").onmouseenter = function() {
+    document.getElementById("settings").style.animation =
+      "spin-quarter-clock 0.1s linear forwards";
+    console.log("triggered");
+  };
+
+  document.getElementById("settings").onmouseleave = function() {
+    document.getElementById("settings").style.animation =
+      "spin-quarter-anti-clock 0.1s linear";
+    console.log("Leave triggered");
+  };
+};
+
+// $("#settings").mouseleave(function(){
+//     $("#settings").style.transform = "rotate(45deg)";
+// });
